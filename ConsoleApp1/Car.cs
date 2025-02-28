3701eb17332cf2a5d223aa6febcd862f74e93625
Никита <153456515+Lairont@users.noreply.github.com>
2025-02-28 12:43:35 +0200

    Add files via upload

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+         string model;
+         int year;
+        private static int totalCars = 0;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year;}
+            set { year = value;}
+        }
+        public Car() { }
+        public Car(string model, int year)
+        {
+            this.Model = model;
+            this.Year = year;
+            totalCars++;
+        }
+        public override string ToString()
+        {
+            return $"{model}\n{year}\n{totalCars}";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car cars1 = new Car("БМВ", 1994);
+            Console.WriteLine($"{cars1.ToString()}");
+            Car cars2 = new Car("Мерседес", 1969);
+            Console.WriteLine($"{cars2.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
