8722f69acc0613bda468960d654c6f620c15974e
Никита <153456515+Lairont@users.noreply.github.com>
2025-03-05 22:48:43 +0200

    Add files via upload

---
+++ ConsoleApp5/Point.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Point
+    {
+        private double x;
+        private double y;
+        private static int count;
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public static int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public Point()
+        {
+            X = 1.0;
+            Y = 1.0;
+            Count++;
+        }
+        static Point()
+        {
+            Count = 0;
+        }
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+            Count++;
+        }
+        public override string ToString()
+        {
+            return $"x= {x} y= {y}\n{count}";
+
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(2.1, 3);
+            Console.WriteLine($"{p1.ToString()}");
+            Point p2 = new Point();
+            Console.WriteLine($"{p2.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
